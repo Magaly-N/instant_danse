f02e9a60ae0544ec4a65e9fa63016c2fc69e4d3b
Magaly N <magalynou@hotmail.fr>
2024-01-31 17:13:40 +0100

    Gitignore

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import initRoutes from "./routes/init.routes.js";
 import initMiddlewares from "./middlewares/init.middleware.js";
 
 const app = express();
-const test = 'test';
 const PORT = process.env.PORT || 9000;
 
 initMiddlewares(app);
